be7db23f344194d5ce8f59a81d637b9b11348ff7
KawalaE <edyta.wer.kawala@gmail.com>
2023-09-25 18:00:08 +0200

    Set up ESLint and Prettier

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+
+export const entry = './src/index.js';
+export const output = {
+    filename: 'main.js',
+    path: resolve(__dirname, 'dist'),
+};
+export const module = {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+    ],
+};
