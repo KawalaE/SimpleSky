a64b46ff55a875e9264c41a5e9f2376103722aa2
KawalaE <edyta.wer.kawala@gmail.com>
2023-09-29 19:41:58 +0200

    Fetch weather data from a city name

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,13 +1,33 @@
 async function getLatLong(city) {
   const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`;
-  const response = await fetch(url, { mode: "cors" });
-  const data = await response.json();
-  const { latitude } = data.results[0];
-  const { longitude } = data.results[0];
-  const geoLocation = [latitude, longitude];
-  return geoLocation;
+  try {
+    const response = await fetch(url, { mode: "cors" });
+    const data = await response.json();
+    if (response.status === 200) {
+      const { latitude } = data.results[0];
+      const { longitude } = data.results[0];
+      const geoLocation = [latitude, longitude];
+      return geoLocation;
+    }
+    console.log("Server error");
+  } catch (error) {
+    console.log("Error", error);
+  }
 }
 
-export default function getWeather() {
+async function getWeather(latitude, longitude) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+  const response = await fetch(url, { mode: "cors" });
+  const data = await response.json();
+  return data;
+}
+
+let coordinates = [];
+getLatLong("Kraków").then((data) => {
+  coordinates = data;
+  getWeather(coordinates[0], coordinates[1]).then((data) => console.log(data));
+});
+
+export default function getForecast() {
   console.log(getLatLong("Berlin"));
 }
