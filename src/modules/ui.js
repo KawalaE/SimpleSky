7b37edf3b1d6c31688a63a3b72fe0ee3fe79c462
KawalaE <edyta.wer.kawala@gmail.com>
2023-09-29 12:31:17 +0200

    Style search bar

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,9 +1,17 @@
 import "../style.css";
 
 import CloudRain from "../assets/pictures/cloud.svg";
+import Magnifier from "../assets/pictures/magnifier.svg";
+import Delete from "../assets/pictures/delete.svg";
 
 const cloudRaining = new Image();
 cloudRaining.src = CloudRain;
+
+const magnifier = new Image();
+magnifier.src = Magnifier;
+
+const deleteX = new Image();
+deleteX.src = Delete;
 
 class UI {
   static createFavicon() {
@@ -23,11 +31,20 @@ class UI {
     const searchBar = document.createElement("input");
     searchBar.id = "search";
     searchBar.type = "text";
+    searchBar.placeholder = "City";
     searchBar.classList.add("search");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("button-delete");
     const searchBtn = document.createElement("button");
+    const xImage = document.createElement("img");
+    xImage.src = deleteX.src;
+    deleteBtn.append(xImage);
     searchBtn.id = "search";
     searchBtn.classList.add("search");
-    searchDiv.append(searchBar, searchBtn);
+    const loupe = document.createElement("img");
+    loupe.src = magnifier.src;
+    searchBtn.append(magnifier);
+    searchDiv.append(deleteBtn, searchBar, searchBtn);
     page.appendChild(searchDiv);
   }
 }
