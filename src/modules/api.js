f7780ded716fcab0de311aea40af882421a69042
KawalaE <edyta.wer.kawala@gmail.com>
2023-10-03 20:48:48 +0200

    Retrive all needed data successfully (manage timezone in api)

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,12 +1,13 @@
 async function getLatLong(city) {
-  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`;
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&timezone&language=en&format=json`;
   try {
     const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
     if (response.status === 200) {
       const { latitude } = data.results[0];
       const { longitude } = data.results[0];
-      const geoLocation = [latitude, longitude];
+      const { timezone } = data.results[0];
+      const geoLocation = [latitude, longitude, timezone];
       return geoLocation;
     }
     console.log("Server error");
@@ -15,8 +16,8 @@ async function getLatLong(city) {
   }
 }
 
-async function getWeather(latitude, longitude) {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+async function getWeather(latitude, longitude, timezone) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,precipitation_probability_mean,snowfall_sum,windspeed_10m_max`;
   const response = await fetch(url, { mode: "cors" });
   const data = await response.json();
   return data;
@@ -25,7 +26,7 @@ async function getWeather(latitude, longitude) {
 let coordinates = [];
 getLatLong("Kraków").then((data) => {
   coordinates = data;
-  getWeather(coordinates[0], coordinates[1]).then((data) => console.log(data));
+  getWeather(coordinates[0], coordinates[1], coordinates[2]).then((data) => console.log(data));
 });
 
 export default function getForecast() {
